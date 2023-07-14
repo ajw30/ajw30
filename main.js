fccee2c094a6d4ad728d2d3bb980e34db5a64dc3
ajw30 <139587896+ajw30@users.noreply.github.com>
2023-07-14 16:23:33 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     let contentDiv = document.getElementById('content');
     
-    let loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultrices condimentum libero, ac ultricies arcu semper sit amet. Integer congue dapibus quam, non pellentesque ante tincidunt a. Donec finibus, ligula a tincidunt luctus, felis massa pellentesque mi, non mollis risus justo quis massa. Nulla facilisi. Morbi pulvinar ex non felis scelerisque interdum. In venenatis vehicula lacus, et condimentum nunc semper a. Vivamus nec urna sed urna semper interdum quis in erat. Suspendisse sed sem vitae augue porta commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin eget lacus velit.';
+    let loremIpsum = 'Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultrices condimentum libero, ac ultricies arcu semper sit amet. Integer congue dapibus quam, non pellentesque ante tincidunt a. Donec finibus, ligula a tincidunt luctus, felis massa pellentesque mi, non mollis risus justo quis massa. Nulla facilisi. Morbi pulvinar ex non felis scelerisque interdum. In venenatis vehicula lacus, et condimentum nunc semper a. Vivamus nec urna sed urna semper interdum quis in erat. Suspendisse sed sem vitae augue porta commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin eget lacus velit.';
 
     for (let i = 0; i < 5; i++) {
         let paragraph = document.createElement('p');
